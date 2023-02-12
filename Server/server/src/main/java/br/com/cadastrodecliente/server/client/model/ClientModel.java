14a7668b11970903d521ffb692b79c5af6b70183
Pedro <pedroprenhaca@hotmail.com>
2023-02-12 15:38:20 -0300

    removido import sem utilidade

--- Server/server/src/main/java/br/com/cadastrodecliente/server/client/model/ClientModel.java
+++ Server/server/src/main/java/br/com/cadastrodecliente/server/client/model/ClientModel.java
@@ -1,7 +1,5 @@
 package br.com.cadastrodecliente.server.client.model;
 
-import org.hibernate.validator.constraints.br.CPF;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
